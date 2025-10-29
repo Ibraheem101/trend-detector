48cc93f0075bdd336963b78ee823d4d910dc045a
ibraheem101 <ayokeks@gmail.com>
2025-10-29 17:41:31 +0300

    feat: implement base hacker news scraper structure

---
+++ services/ingestion/ingestion.py
@@ -0,0 +1,35 @@
+#type: ignore
+import os
+import sys
+import requests
+from sqlalchemy.orm import Session
+from datetime import datetime, timezone
+
+from shared.database import Rawpost, SessionLocal
+
+class HackerNewsScraper():
+    """"""
+    BASE_URL = "https://hacker-news.firebaseio.com/v0"
+
+    def __init__(self):
+        self.session = requests.Session()
+    
+    def get_top_stories(self, limit: int = 50) -> list[int]:
+        """"""
+        url = f"{self.BASE_URL}/topstories.json"
+        response = self.session.get(url)
+        response.raise_for_status()
+        story_ids = response.json()
+        return story_ids[:limit]
+    
+    def get_story_details(self, story_id: int) -> dict:
+        """"""
+        url = f"{self.BASE_URL}/item/{story_id}.json"
+        response = self.session.get(url)
+        response.raise_for_status()
+        return response.json()
+    
+    def save_to_database():
+        """"""
+        return None
+
